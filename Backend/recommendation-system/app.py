bbb0eea947fcae8bf43de98cd5db00755c0dc4cc
Nguyễn Sang <sangnn8@gmail.com>
2024-10-11 10:24:13 +0700

    feat: recommend books by content, recommend books by similar users

--- Backend/recommendation-system/app.py
+++ Backend/recommendation-system/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS 
-from ai.recommendation_model import update_model,recommend_books
+from ai.recommendation_model import update_model,recommend_books,get_recommendations
 from bson import ObjectId
 
 app = Flask(__name__)
@@ -18,7 +18,17 @@ def update_or_create_model():
 
 @app.route('/recommend_books/<user_id>', methods=['GET'])
 def recommend_books_route(user_id):
-    recommendations = recommend_books(user_id)
-    return jsonify(recommendations), 200
+    try:
+        recommendations = recommend_books(user_id)
+        return jsonify(recommendations), 200
+    except Exception as e:
+        return jsonify({'message': str(e)}), 404
+@app.route('/recommend_books_by_majors/<user_id>', methods=['GET'])
+def recommend_books_by_majors_route(user_id):
+    try:
+        recommendations = get_recommendations(user_id)
+        return jsonify(recommendations), 200
+    except Exception as e:
+        return jsonify({'message': str(e)}), 404
 if __name__ == '__main__':
     app.run(debug=True)
