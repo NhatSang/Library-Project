719ab227d09a3b34fc783128559172f862a4b8eb
PhamDucNhan <phamducnhan.it@gmail.com>
2024-10-27 14:21:19 +0700

    #Edit api craete history-view

--- Backend/library-server/controllers/ViewController.js
+++ Backend/library-server/controllers/ViewController.js
@@ -1,5 +1,9 @@
 import View from "../models/View.js";
 
-export const updateView = async (bookId) => {
-
+export const updateView = async (book) => {
+    try {
+        await View.updateOne({ book }, { $inc: { viewCount: 1 } }, { upsert: true });
+    } catch (error) {
+        console.log(error);
+    }
 };
--- Backend/library-server/controllers/book_controller/BookController.js
+++ Backend/library-server/controllers/book_controller/BookController.js
@@ -371,3 +371,4 @@ export const searchBooksByName = async (req,res) => {
     });
   }
 }
+
